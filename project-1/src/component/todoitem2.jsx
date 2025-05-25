17ef2591a187a810f9c03ccf1296475db1ec6a0c
nikhilranjan01 <nikhilranjan2004@gmail.com>
2025-05-25 21:22:00 +0530

    New Update

--- project-1/src/component/todoitem2.jsx
+++ project-1/src/component/todoitem2.jsx
@@ -6,6 +6,7 @@ function Todoitem2() {
       <div class="row">
         <div class="col-6">{todoname}</div>
         <div class="col-4">{tododate}</div>
+        <div class="col-4">{tododate}</div>
         <div class="col-2">
           <button type="button" class="btn btn-danger">
             delete
